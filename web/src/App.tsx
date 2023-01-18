3f1e3c0562267f319a9acd74d1fe93f157f12607
Lucas Gonçalves <lucas.gabriel.goncalves12@gmail.com>
2023-01-18 14:31:34 -0300

    feat: create componens Header, HabbitDay and SummaryTable

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,13 @@
-import { Habit } from "./components/Habit";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 
 export function App() {
   return (
-    <div className="w-screen h-screen bg-violet-800 flex items-center justify-center gap-1">
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
