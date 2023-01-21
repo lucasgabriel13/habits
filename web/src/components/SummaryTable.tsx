dda933ad0306133a10a8786454751c40b232e5b5
Lucas Gonçalves <lucas.gabriel.goncalves12@gmail.com>
2023-01-21 10:13:25 -0300

    feat: made connection with the backend

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,5 +1,8 @@
-import { generateDateFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
-import { HabitDay } from "./HabitDay";
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+import { api } from '../lib/axios';
+import { generateDateFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
+import { HabitDay } from './HabitDay';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 
@@ -8,7 +11,22 @@ const summaryDate = generateDateFromYearBeginning();
 const minimumSummaryDatesSize = 18 * 7 //18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDate.length;
 
+type SummaryData = {
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}[];
+
 export function SummaryTable() {
+  const [summary, setSummary] = useState<SummaryData>([]);
+
+  useEffect(() => {
+    api.get('summary').then(response => {
+      setSummary(response.data)
+    });
+  }, []);
+
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -23,11 +41,16 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDate.map(date => {
+          const dayInSummary = summary.find(day => {
+            return dayjs(date).isSame(day.date, 'day');
+          });
+
           return (
             <HabitDay
               key={date.toString()}
-              amount={5}
-              completed={Math.round(Math.random() * 5)}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
             />
           );
         })}
@@ -40,6 +63,7 @@ export function SummaryTable() {
             />
           );
         })}
+
       </div>
     </div >
   );
